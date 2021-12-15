7f9071023a9a1bee86c0f1c14f1b63fcdd3d303c
beni69 <73666440+beni69@users.noreply.github.com>
2021-12-15 17:47:34 +0100

    :tada: first commit

---
+++ src/main.rs
@@ -0,0 +1,159 @@
+#[macro_use]
+extern crate log;
+use isahc::{
+    cookies::{Cookie, CookieJar},
+    http::Uri,
+    prelude::*,
+    Error, Request,
+};
+use serde_derive::Deserialize;
+use std::{
+    env::args,
+    fs,
+    path::PathBuf,
+    process::{exit, Command},
+    time::SystemTime,
+};
+
+const HOST: &str = "https://szlginfo.ptamas.hu";
+
+#[derive(Debug, Deserialize)]
+struct Conf {
+    username: String,
+    password: String,
+}
+
+fn main() -> Result<(), Error> {
+    // setup logger
+    pretty_env_logger::init();
+    info!("starting up..");
+
+    // parse args
+    let args: Vec<String> = args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <file url> <file name>", args[0]);
+        exit(1);
+    }
+    let file_url = &args[1];
+    let file_path = &args[2];
+    // parse config file
+    let mut conf_file = PathBuf::from(dirs::config_dir().unwrap());
+    conf_file.push("mentordl.txt");
+    info!("looking for config at {:?}", &conf_file);
+    let conf_str = match fs::read_to_string(&conf_file) {
+        Ok(f) => f,
+        Err(_) => {
+            eprintln!(
+                "Config file not found. Put your username and password in this file: {:?}",
+                &conf_file
+            );
+            exit(1);
+        }
+    };
+    let conf: Conf = match toml::from_str(&conf_str) {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("Config file at {:?} is invalid. Please do:\nusername = 'your username'\npassword = 'your password'", &conf_file);
+            exit(1);
+        }
+    };
+    info!("{:?}", &conf);
+
+    // create a session
+    let cookie_jar = CookieJar::new();
+    let uri: Uri = "https://szlginfo.ptamas.hu/login.php".parse().unwrap();
+    let mut res1 = Request::get(&uri)
+        .cookie_jar(cookie_jar.clone())
+        .body(())?
+        .send()?;
+
+    // parse the session data
+    let res1_text = res1.text()?;
+    let csrf_pos = res1_text
+        .match_indices("const csrf = '")
+        .next()
+        .expect("csrf not found")
+        .0;
+    info!("{:?}", &csrf_pos);
+    let csrf = &res1_text[csrf_pos + 14..csrf_pos + 14 + 64];
+    info!("{:?}", &csrf);
+
+    let sess: Cookie = cookie_jar
+        .get_for_uri(&HOST.parse().unwrap())
+        .into_iter()
+        .find(|c| c.name() == "PHPSESSID")
+        .unwrap();
+    info!("{:?}", &sess);
+
+    // login
+    let uri2: Uri = format!("{}/backend/login.php", HOST).parse().unwrap();
+    let mut _res2 = Request::post(&uri2)
+        .cookie_jar(cookie_jar.clone())
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(format!(
+            "username={}&password={}&csrf={}",
+            &conf.username, &conf.password, csrf
+        ))?
+        .send()?;
+
+    // download the file
+    let mut url_file = url::Url::parse(file_url).expect("invalid url");
+    let query = url_file.query().expect("invalid url");
+    let mut q_str = String::new();
+    for q in query.split('&') {
+        let q = &q.replace("\\", ""); // some terminals do some weird character escaping
+        if q.starts_with("csrf") {
+            q_str.push_str(&format!("csrf={}", &csrf));
+            q_str.push('&');
+        } else {
+            q_str.push_str(q);
+            q_str.push('&');
+        }
+    }
+    info!("{}", &q_str[..q_str.len() - 1]);
+    url_file.set_query(Some(&q_str[..q_str.len() - 1]));
+    let uri_file: Uri = url_file.as_str().parse().unwrap();
+    debug!("{:?}", &uri_file);
+    let mut _res_file = Request::get(&uri_file)
+        .cookie_jar(cookie_jar.clone())
+        .body(())?
+        .send()?;
+
+    let zipname = format!("mentordl_tmp_{}_{}.zip", &file_path, get_unix_time());
+    _res_file.copy_to_file(&zipname)?;
+
+    // unzip the file
+    #[cfg(unix)]
+    {
+        exec(format!("unzip {} -d {}", &zipname, &file_path));
+    }
+    #[cfg(windows)]
+    {
+        exec(format!("powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -Command 'Expand-Archive -DestinationPath {} -LiteralPath {}'", &file_path, &zipname));
+    }
+
+    // delete original zip file
+    fs::remove_file(&zipname).expect("failed to remove zip file, but everyehing else went fine.");
+
+    Ok(())
+}
+fn exec(cmd: String) {
+    info!("executing command: {}", &cmd);
+    let cmd = string_to_cmd_and_args(&cmd);
+    let out = Command::new(cmd.0)
+        .args(cmd.1)
+        .status()
+        .expect("failed to spawn unzip command");
+    info!("zip extractor exited with {}", out);
+}
+pub fn string_to_cmd_and_args(s: &str) -> (&str, Vec<&str>) {
+    let v = s.split_ascii_whitespace().collect::<Vec<&str>>();
+    let first = v.split_first().unwrap_or((&"", &[]));
+    (*first.0, first.1.to_vec())
+}
+pub fn get_unix_time() -> u128 {
+    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(t) => t.as_millis(),
+        Err(_) => 0,
+    }
+}
